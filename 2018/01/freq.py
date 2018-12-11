62ac4441770e120b80e32632607cf50c6190ea6a
David Chin <david.w.h.chin@gmail.com>
2018-12-11 13:37:34 -0500

    debug stmt

--- 2018/01/freq.py
+++ 2018/01/freq.py
@@ -4,9 +4,11 @@ import os
 
 # starting frequency 0
 f0 = 0
+print(f0)
 with open('input.txt', 'r') as f:
     for l in f:
         f0 += int(l.strip())
+        print(f0)
 
 print("final freq = {}".format(f0))
 
