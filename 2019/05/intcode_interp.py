7f1251731bbb3cbbf5412e1c1ea298012a94434e
David Chin <david.w.h.chin@gmail.com>
2019-12-10 11:56:24 -0500

    Small refactor

--- 2019/05/intcode_interp.py
+++ 2019/05/intcode_interp.py
@@ -16,10 +16,23 @@ def two(i1, i2, i3):
     prog[prog[i3]] = prog[prog[i1]] * prog[prog[i2]]
 
 
+def interp():
+    global prog
+
+    for c in range(0, len(prog), 4):
+        if prog[c] == 1:
+            one(c+1, c+2, c+3)
+            continue
+        elif prog[c] == 2:
+            two(c+1, c+2, c+3)
+            continue
+        elif prog[c] == 99:
+            break
+
 def main():
     global prog
     
-    parser = argparse.ArgumentParser("AoC 2.1")
+    parser = argparse.ArgumentParser("AoC 5")
     parser.add_argument('infile', metavar='infile', type=str, nargs=1, help='Input file')
 
     args = parser.parse_args()
@@ -31,15 +44,7 @@ def main():
 
     print("prog = {}".format(prog))
 
-    for c in range(0, len(prog), 4):
-        if prog[c] == 1:
-            one(c+1, c+2, c+3)
-            continue
-        elif prog[c] == 2:
-            two(c+1, c+2, c+3)
-            continue
-        elif prog[c] == 99:
-            break
+    interp()
 
     print("result = {}".format(prog))
 
