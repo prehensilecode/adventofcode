a728c6610c67360ed6bb5a6bcca0fcf98cc83428
David Chin <david.w.h.chin@gmail.com>
2021-05-16 00:01:14 -0400

    Day 3 pt 1

---
+++ 2020/03.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+def vecsum(a, b):
+    global width
+    global height
+
+    return [(a[0] + b[0]) % width, a[1] + b[1]]
+
+map = []
+with open('input03', 'r') as f:
+    for l in f:
+        map.append(l.strip())
+
+
+width = len(map[0])
+height = len(map)
+print('width = {}, height = {}'.format(width, height))
+
+directions = [3, 1]
+pos = [0, 0]
+num_trees = 0
+for rowcount in range(height-1):
+    new_pos = vecsum(pos, directions)
+    #print('FOO', pos, new_pos)
+    if map[new_pos[1]][new_pos[0]] == '#':
+        num_trees += 1
+
+    pos = new_pos.copy()
+
+print('Num. trees = {}'.format(num_trees))
+
