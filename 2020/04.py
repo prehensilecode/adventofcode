dfcad8c0cac9ce8b1b840106ec56c90bb5a23281
David Chin <david.w.h.chin@gmail.com>
2021-05-16 15:59:33 -0400

    More work on Day 4 pt 2

--- 2020/04.py
+++ 2020/04.py
@@ -28,11 +28,13 @@ def valid_passport(passport):
 # iyr: 4 digit, [2010, 2020]
 # eyr: 4 digit, [2020, 2030]
 def valid_yr(yr, min, max):
+    print('FOOBAR YEAR: ', yr, min, max)
     return (len(yr) == 4) and (int(yr) >= min) and (int(yr) <= max)
 
 def valid_hgt(hgt):
     valid_p = None
     height, unit = int(hgt[:-2]), hgt[-2:]
+    print('FOOBAR HEIGHT, UNIT: ', height, unit)
     if unit == 'cm':
         valid_p = (height >= 150) and (height <= 193)
     elif unit == 'in':
@@ -46,15 +48,19 @@ def valid_hcl(hcl):
     return (hcl[0] == '#') and (re.match(hex_pat, hcl[1:]))
 
 def valid_ecl(ecl):
+    print('FOOBAR EYE COLOR:', ecl)
     return ecl in set(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
 
 def valid_pid(pid):
-    return re.match(r'\d{9}', pid)
+    print('FOOBAR PID:', pid)
+    return re.match(r'(\d{9})', pid)
 
 def strict_valid_passport(p):
     if valid_passport(p):
+        print('LOOSE VALID')
         return valid_yr(p['byr'], 1920, 2002) and valid_yr(p['iyr'], 2010, 2020) and valid_yr(p['eyr'], 2020, 2030) and valid_hgt(p['hgt']) and valid_hcl(p['hcl']) and valid_ecl(p['ecl']) and valid_pid(p['pid'])
     else:
+        print('LOOSE INVALID')
         return False
 
 
@@ -93,12 +99,12 @@ passports = read_passports('input04')
 print('Read {} passports'.format(len(passports)))
 n_valid = 0
 for p in passports:
-    #print(p)
     if valid_passport(p):
-        print('VALID', p)
+        #print('VALID', p)
         n_valid += 1
     else:
-        print('INVALID', p)
+        #print('INVALID', p)
+        pass
 
 print(f'{n_valid} valid passports')
 print('')
