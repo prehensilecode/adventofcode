6cf80c24a5a52c14d7bd54d24238278a56e59171
David Chin <david.w.h.chin@gmail.com>
2021-12-19 01:30:36 -0500

    Use BitVector

--- 2021/03/03.py
+++ 2021/03/03.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from BitVector import BitVector
 
 def gamma_epsilon(loglines):
     res = []
@@ -10,7 +11,7 @@ def gamma_epsilon(loglines):
     for n in range(npos):
         for l in loglines:
             s += int(l[n])
-        if s < float(nlines)/2.:
+        if s < nlines/2.:
             res.append('0')
         else:
             res.append('1')
@@ -26,13 +27,40 @@ def gamma_epsilon(loglines):
     return int(''.join(gamma_bin), 2), int(''.join(epsilon_bin), 2)
 
 
+def dg(loglines):
+    data = []
+    for line in loglines:
+        data.append(BitVector(bitstring=line.strip()))
+
+    nlines = len(data)
+    npos = len(loglines[0])
+
+    # list of 0th elems
+    res = []
+    for n in range(npos):
+        nths = [int(el[n]) for el in data]
+        if sum(nths) < nlines/2.:
+            res.append(0)
+        else:
+            res.append(1)
+    gamma_bin = BitVector(bitlist=res)
+    epsilon_bin = ~gamma_bin
+
+    return gamma_bin.int_val(), epsilon_bin.int_val()
+
+
 loglines = []
-with open('input1.txt', 'r') as f:
+with open('test1.txt', 'r') as f:
     for l in f:
         loglines.append(l.strip())
 
-ge = gamma_epsilon(loglines)
-print(ge)
-power = ge[0] * ge[1]
+g, e = gamma_epsilon(loglines)
+print(f'g, e = {g}, {e}')
+power = g * e
 print(f'Power = {power}')
+print('')
 
+print('Using BitVector')
+g, e = dg(loglines)
+print(f'g, e = {g}, {e}')
+print(f'Power = {g *  e}')
