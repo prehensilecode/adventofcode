7d0de6f049b418d2e875aba636447ad182ab5dc3
David Chin <david.w.h.chin@gmail.com>
2022-12-04 22:06:59 -0500

    something

--- 2020/01.py
+++ 2020/01.py
@@ -4,18 +4,18 @@ import os
 
 numbers = []
 with open('input01', 'r') as f:
-    for l in f:
-        numbers.append(int(l.strip()))
+    for line in f:
+        numbers.append(int(line.strip()))
 
 count = 0
 for count in range(len(numbers)):
-    for m in numbers[count+1:]:
+    for m in numbers[count + 1:]:
         if numbers[count] + m == 2020:
             print(numbers[count] * m)
 
 count = 0
 for count in range(len(numbers)):
-    for m in numbers[count+1:]:
-        for n in numbers[count+2:]:
+    for m in numbers[count + 1:]:
+        for n in numbers[count + 2:]:
             if numbers[count] + m + n == 2020:
                 print(numbers[count] * m * n)
