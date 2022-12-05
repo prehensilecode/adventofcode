83beab20a881d235052f507f230990618e782efc
David Chin <david.w.h.chin@gmail.com>
2022-12-04 23:25:53 -0500

    Day 3

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -6,18 +6,33 @@ higher_prio = [n for n in range(27, 53)]
 lower_alpha = [chr(n) for n in range(ord('a'), ord('z') + 1)]
 higher_alpha = [chr(n) for n in range(ord('A'), ord('Z') + 1)]
 
-print(f'lower_prio = {lower_prio}')
-print(f'higher_prio = {higher_prio}')
-print(f'lower_alpha = {lower_alpha}')
-print(f'higher_alpha = {higher_alpha}')
+#print(f'lower_prio = {lower_prio}')
+#print(f'higher_prio = {higher_prio}')
+#print(f'lower_alpha = {lower_alpha}')
+#print(f'higher_alpha = {higher_alpha}')
 
 lower = dict(zip(lower_alpha, lower_prio))
 higher = dict(zip(higher_alpha, higher_prio))
+priorities = lower | higher
 
-print(lower)
-print(higher)
+#print(lower)
+#print(higher)
+print(priorities)
 
 with open('test.txt', 'r') as infile:
+    common_items = []
     for line in infile:
         line_length = len(line.strip())
-        print(line_length, line.strip(), line[:line_length//2], line[line_length//2:-1])
+        print(line_length, line.strip(), line.strip()[:line_length//2], line.strip()[line_length//2:])
+        left = set(line.strip()[:line_length//2])
+        right = set(line.strip()[line_length//2:])
+        intersection = left & right
+        print(intersection)
+        if intersection:
+            for i in list(intersection):
+                common_items.append(i)
+
+print(common_items)
+prio_sum = sum([priorities[i] for i in common_items])
+print(prio_sum)
+
